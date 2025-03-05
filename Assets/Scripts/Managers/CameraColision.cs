8487bdf43a2e3d2c0b08c6d7f0cf81d1b036375c
Ivansavv <gif.savvateev@gmail.com>
2025-03-05 15:41:28 +0100

    script aded CameraColission

---
+++ Assets/Scripts/Managers/CameraColision.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraColision : MonoBehaviour
+{
+    int[] dangerousObjects;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (GameObject MainCamera )
+    }
+}   
